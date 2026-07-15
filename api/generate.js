d7b07006f0ebe050c6f8fc95aa42d40bcf7e5cea
nwreey <moexmustafa@gmail.com>
2026-07-15 08:58:53 +0300

    LL

--- api/generate.js
+++ api/generate.js
@@ -63,7 +63,7 @@ module.exports = async (req, res) => {
       body: JSON.stringify({
         model: 'gpt-4o',
         messages: openaiMessages,
-        max_tokens: max_tokens || 2000,
+        max_tokens: max_tokens || 4096,
       }),
     });
 
